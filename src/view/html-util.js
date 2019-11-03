0384cb621457da825370abbf8e59339cd71b0904
melty12 <komei.1220@gmail.com>
2019-11-03 15:55:41 +0900

    first commit

---
+++ src/view/html-util.js
@@ -0,0 +1,39 @@
+export function escapeSpecialChars(str)
+{ 
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+export function html2Element(html)
+{ 
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
+
+export function element(strings, ...values)
+{ 
+  const htmlString = strings.reduce((result, str, i) =>
+  {
+    const value = values[i - 1];
+
+    if (typeof value === "string")
+    {
+      return result + escapeSpecialChars(value) + str;
+    } else
+    {
+      return result + String(value) + str;
+    }
+  });
+  return html2Element(htmlString);
+}
+
+export function render(bodyElement, containerElement)
+{
+  containerElement.innerHTML = "";
+  containerElement.appendChild(bodyElement);
+}
